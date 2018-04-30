7db93fe22ee31049fc7867f8417ea29f9c38e7c6
Vyacheslavick <37550293+Vyacheslavick@users.noreply.github.com>
2018-04-30 14:32:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println(Generator.generate());
         UkrainianWeather weather = new UkrainianWeather();
         EnglishWeather englishWeather = new EnglishWeather();
+        RussianWeather russianWeather = new RussianWeather();
+        russianWeather.start();
         weather.start();
         Thread.sleep(4000);
         englishWeather.start();
